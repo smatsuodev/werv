2a6be1c9a71cfbd9c51040c0e25a194334d1ba58
EringiShimeji <eringi.shimeji.5296@gmail.com>
2023-01-21 17:58:25 +0900

    add compile file mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::Read;
+
 fn main() {
+    let mut args = std::env::args();
+
+    if let Some(path) = args.nth(1) {
+        let mut file = std::fs::File::open(path).unwrap();
+        let mut input = String::new();
+
+        file.read_to_string(&mut input).unwrap();
+
+        let result = wervc::parse(&input).unwrap();
+
+        println!("{}", result);
+        return;
+    }
+
     repl::start();
 }
